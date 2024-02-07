1c9d877226c8f0d986297ab521df9c0a6478960f
Mohammad Khadeer <mkhadeer@nisum.com>
2024-02-07 14:10:20 +0530

    episode-03

--- App.js
+++ App.js
@@ -2,10 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 
-const heading = React.createElement(
-  "h1",
-  { id: "heading" },
-  "Hello World from React!"
+const Title =()=> (
+  <h1 className="head">Namasthe React Jsx Using</h1>
 );
+
+const HeadingComponent = ()=> (
+ 
+  <div id ="container">
+  <Title/>
+  <h1 className="heading">Namasthe React functional Components </h1></div>
+)
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(<HeadingComponent/>);
